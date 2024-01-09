ecb0bde64ae730125d3f9603bfb50e169e6d3759
hsun0987 <116503534+hsun0987@users.noreply.github.com>
2024-01-09 15:46:54 +0900

    Revert "Feature/#5 jdbc"

--- src/main/java/com/kitri/myservletboard/service/BoardService.java
+++ src/main/java/com/kitri/myservletboard/service/BoardService.java
@@ -1,16 +1,13 @@
 package com.kitri.myservletboard.service;
 
 import com.kitri.myservletboard.dao.BoardDao;
-import com.kitri.myservletboard.dao.BoardJdbcDao;
 import com.kitri.myservletboard.dao.BoardMemoryDao;
 import data.Board;
 
 import java.util.ArrayList;
 
 public class BoardService {
-   // BoardDao boardDao = BoardMemoryDao.getInstance();
-
-    BoardDao boardDao = BoardJdbcDao.getInstance();
+    BoardDao boardDao = BoardMemoryDao.getInstance();
 
     // 싱글톤
     private BoardService() {};
