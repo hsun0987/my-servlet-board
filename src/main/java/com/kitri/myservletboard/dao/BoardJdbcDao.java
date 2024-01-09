bf08979099b0195f29ed3e5b42e905e8f4b79bf6
MinSun <hsun0987@daum.net>
2024-01-09 14:52:47 +0900

    commit 7
    - JDBC DB 연동 및 게시글 리스트 등록/수정/삭제 기능 구현

--- src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
+++ src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
@@ -70,16 +70,114 @@ public class BoardJdbcDao implements BoardDao{
 
         return boards;
     }
-    public Board getById(Long id){
-        return null;
-    }
-    public void save(Board board){
+    public Board getById(Long id){  // DB ->
+        // connection
+        //ps -> executeQuery()
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Board board1 = null;
 
-    }
-    public void update(Board board){
+        try {
+            connection = connectionDB();
+            String sql = "SELECT * FROM board WHERE id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1, id);
+            rs = ps.executeQuery();
 
+            rs.next();
+            Long id1 =  rs.getLong("id");
+            String title = rs.getString("Title");
+            String content = rs.getString("content");
+            String writer = rs.getString("writer");
+            LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+            int viewCount = rs.getInt("view_count");
+            int commentCount = rs.getInt("comment_count");
+
+            board1 = new Board(id1, title, content, writer, createdAt, viewCount, commentCount);
+
+        }catch(Exception e){
+
+        }finally {
+            try {
+                rs.close();
+                ps.close();
+                connection.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        return board1;
+    }
+    public void save(Board board){  // DB <-
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = connectionDB();
+            String sql = "INSERT INTO board(title, content, writer) VALUES (?, ?, ?)";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContent());
+            ps.setString(3, board.getWriter());
+            ps.executeUpdate();
+
+        }catch (Exception e){
+
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    public void update(Board board){    // DB <-
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = connectionDB();
+            String sql = "UPDATE board SET title = ?, content = ? WHERE id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, board.getTitle());
+            ps.setString(2, board.getContent());
+            ps.setLong(3, board.getId());
+            ps.executeUpdate();
+
+        }catch (Exception e){
+
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
     public void delete(Board board){
+        Connection connection = null;
+        PreparedStatement ps = null;
 
+        try {
+            connection = connectionDB();
+            String sql = "DELETE FROM board WHERE id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1, board.getId());
+            ps.executeUpdate();
+
+        }catch (Exception e){
+
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
